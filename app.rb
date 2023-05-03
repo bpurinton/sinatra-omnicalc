997de6be10a998e600dd8db3691de7c834f9b886
Ben Purinton <ben.purinton@gmail.com>
2023-05-02 17:59:21 -0800

    first step

--- app.rb
+++ app.rb
@@ -3,8 +3,9 @@
 require 'sinatra'
 
 get('/') do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  redirect('/square/new')
+end
+
+get('/square/new') do
+  erb(:square_form)
 end
