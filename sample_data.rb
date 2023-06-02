74c460743a398af71cec58bcf3853bafb9dec4f6
Ben Purinton <ben.purinton@gmail.com>
2023-06-02 11:51:46 -0800

    10,000 active record objects

--- sample_data.rb
+++ sample_data.rb
@@ -3,7 +3,7 @@ require './config/environment'
 Task.delete_all
 
 count = 0
-while count < 1000 do
+while count < 10000 do
   Task.create!(name: "name#{count}", description: "description#{count}")
   count += 1
 end
